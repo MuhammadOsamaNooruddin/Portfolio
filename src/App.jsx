4634bfc90441d6eaf641405ab127a3be21411c6c
Muhammad Osama Noor Uddin <usamanooruddin3@gmail.com>
2025-07-04 18:21:47 +0100

    apply changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-navy">Hello, World!</h1>
+      <h1 className="text-4xl font-bold text-navy">Portfolio</h1>
     </div>
   );
 }
