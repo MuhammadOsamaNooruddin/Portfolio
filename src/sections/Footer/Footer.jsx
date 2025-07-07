9a89e1f63cb27abf318cf83980c2a0c861e41282
Muhammad Osama Noor Uddin <usamanooruddin3@gmail.com>
2025-07-07 22:46:36 +0100

    updates changes

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-4 mt-20">
+        <footer className="bg-[#121212] text-white py-4 mt-20">
             <div className="container mx-auto text-center">
                 <p className="text-sm">© {new Date().getFullYear()} Muhammad Osama Noor Uddin. All rights reserved.</p>
                 <p className="text-xs mt-2">Built with ❤️ using React</p>
